882068b5fd2050064e330de5838b398f84c7e446
MonkeyLovers69 <137757068+MonkeyLovers69@users.noreply.github.com>
2023-07-02 23:25:32 +0500

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,9 @@
+print("Welcome its program to calculate. first will calculate square second will calculate Perime(of rectangle)")
+a = float(input())
+b = float(input())
+S = a * b
+print(S)
+a2 = float(input())
+b2 = float(input())
+P = (a2 + b2) * 2
+print(P)
